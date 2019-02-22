3028d04262a856239cbb17f35eb0528da0e598ff
Joern Barthel <joern.barthel@kreuzwerker.de>
2019-02-22 09:10:06 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,51 @@
+# AWS Principal Token (`aptk`)
+
+An `aptk` is a simple implementation of the extraction (which should be
+portable among SDK implementations) and parsing logic for the authorization
+parts of an STS `GetCallerIdentity` call. This makes it possible to implement
+flows where Alice can prove to Bob that can she act as a certain principal (e.g.
+a role or an IAM user).
+
+For this Alice extracts the `Authorization` and `Date` headers from a signed request
+and passes those to Bob. Bob builds his own request, exchanges thoses headers
+and calls STS with the SDK. Depending on the principal, the results should
+match the [regular outcomes from API](https://docs.aws.amazon.com/STS/latest/APIReference/API_GetCallerIdentity.html) yielding
+information about the principal type, account ID and possibly the username
+(depending on the principal type).
+
+## Usage in `go`
+
+```
+package main
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sts"
+	"github.com/yawn/aptk"
+)
+
+func main() {
+
+	client := sts.New(session.New(&aws.Config{}))
+
+	// Alice prepares the call and extracts the appropriate headers
+	t, err := aptk.Prepare(client)
+
+	if err != nil {
+		panic(err)
+	}
+
+	// Bob receives those headers, builds a *Token and parses them
+	res, err := aptk.Parse(client, t)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(*res)
+
+}
+```
---
+++ aptk.go
@@ -0,0 +1,92 @@
+package aptk
+
+import (
+	"github.com/aws/aws-sdk-go/aws/corehandlers"
+	"github.com/aws/aws-sdk-go/service/sts"
+	"github.com/pkg/errors"
+)
+
+const (
+	errFailedToCreateSignature       = "failed to preapre signature for request"
+	errFailedToRequestCallerIdentity = "failed to request caller identity"
+	pncAPIChangedTooMuch             = `"Authorization" header is set despite removing signer from chain - this library is broken now`
+)
+
+const (
+	// Authorization denotes the key for the Authorization header of a POST request to GetCallerIdentity
+	Authorization = "Authorization"
+
+	// Date denotes the key for the Date header of a POST request to GetCallerIdentity
+	Date = "X-Amz-Date"
+)
+
+// Tuple represents the Authorization tuple requires by Parse(). Note that this
+// requires extracting the appropriate values from a signed request object, NOT
+// from a presigned URL which differ structurally (e.g. through different signed
+// headers such as content length and type, having no body body etc.) and there
+// require different signatures.
+//
+// Example for an Authorization value: AWS4-HMAC-SHA256 Credential=AKIAJW.../20190221/us-east-1/sts/aws4_request, SignedHeaders=content-length;content-type;host;x-amz-date, Signature=abcd...
+// Example for a Date value: 20190221T211905Z
+type Tuple struct {
+	Authorization string
+	Date          string
+}
+
+// Parse will get an external callers identity based on the given authorization
+// tuple.
+func Parse(client *sts.STS, tuple *Tuple) (*sts.GetCallerIdentityOutput, error) {
+
+	var headers = map[string]string{
+		Authorization:  tuple.Authorization,
+		"Content-Type": "application/x-www-form-urlencoded; charset=utf-8",
+		Date:           tuple.Date,
+	}
+
+	req, res := client.GetCallerIdentityRequest(nil)
+
+	// the alternative here would have been to "just" remove "v4.SignRequestHandler" - this choice basically makes this usage safer against a renaming of the v4 signer / new signer additions and less safe towards the addition of critical future core handlers
+
+	req.Handlers.Sign.Clear()
+	req.Handlers.Sign.SetFrontNamed(corehandlers.BuildContentLengthHandler)
+
+	{
+
+		// make extra sure we're not signing with a local principal here
+
+		req.Sign()
+
+		if auth := req.HTTPRequest.Header.Get(Authorization); auth != "" {
+			panic(pncAPIChangedTooMuch)
+		}
+
+	}
+
+	for k, v := range headers {
+		req.HTTPRequest.Header.Set(k, v)
+	}
+
+	if err := req.Send(); err != nil {
+		return nil, errors.Wrapf(err, errFailedToRequestCallerIdentity)
+	}
+
+	return res, nil
+
+}
+
+// Prepare extracts the tuple required for Parse. This should be easily
+// reproducable in other SDK environments.
+func Prepare(client *sts.STS) (*Tuple, error) {
+
+	req, _ := client.GetCallerIdentityRequest(nil)
+
+	if err := req.Sign(); err != nil {
+		return nil, errors.Wrapf(err, errFailedToCreateSignature)
+	}
+
+	return &Tuple{
+		Authorization: req.HTTPRequest.Header.Get(Authorization),
+		Date:          req.HTTPRequest.Header.Get(Date),
+	}, nil
+
+}
